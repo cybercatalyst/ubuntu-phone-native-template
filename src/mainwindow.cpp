b60528e621a0aae673bb1bae7e2dde737a5310f1
Jacob Dawid <jacob.dawid@cybercatalyst.net>
2014-05-09 18:09:03 +0200

    Added license headers and simplified file names.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright: 2014 Jacob Dawid <jacob.dawid@cybercatalyst.net>
+ *
+ * This file is part of ubuntu-phone-native-template
+ *
+ * ubuntu-phone-native-template is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * ubuntu-phone-native-template is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Jacob Dawid <jacob.dawid@cybercatalyst.net>
+ *
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
